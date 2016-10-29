331c610e770ff87aeb0ef2c13c26f23290223986
ICHARM <icharm.me@outlook.com>
2016-10-30 00:25:14 +0800

    添加注释

--- inc/config.php
+++ inc/config.php
@@ -23,23 +23,22 @@ $gvarconf = array(
 // db
 $gdbconf = array(
 	'stock' => array(
-		'host' => '121.42.48.161',
-		'port' => 3306,
-		'username' => 'stocktips',
-		'password' => '2s6K1zh9KyalyBzS',
-		'dbname' => 'stocktips',
+		'host' => '172.0.0.1',		//数据库地址
+		'port' => 3306,				//数据库端口
+		'username' => 'username',	//数据库登录用户名
+		'password' => 'password',	//登录密码
+		'dbname' => 'dbname',		//本条配置项对应的数据库名
 	),
 );
-//test
 
 // mail
 $mailconf = array(
 	'foxmail' => array(
-		'host' => 'smtp.qq.com',
-		'port' => 465,
-		'secure' => 'ssl',	//加密方式
+		'host' => 'smtp.qq.com',			//stmp发件地址
+		'port' => 465,						//端口
+		'secure' => 'ssl',					//加密方式
 		'username' => 'icharm.me@foxmail.com',
-		'password' => 'cjdvdeetatsydcic', //qq授权码
+		'password' => 'qq shou quan ma', //qq授权码(现在是使用授权码来代替qq密码)
 		'formName' => '股票预警(ICHARM)', //显示在源邮件地址旁边的head
 	),
 );
